ec5386a2f6a19fc08370750b991d397e97a002cf
Paul <paalge@gmail.com>
2016-08-26 13:03:14 +0200

    Removed unecessary imports

--- PASKIL/PASKIL/misc.py
+++ PASKIL/PASKIL/misc.py
@@ -16,9 +16,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with PASKIL.  If not, see <http://www.gnu.org/licenses/>.
-import contextlib
-import sys
-import warnings
+
 """
 Module containing miscellaneous functions used internally by PASKIL
 
